9a475b783f1a6e77e3469795187f23cda5094a58
pax-ad <ad007scrutinize@gmail.com>
2026-06-20 00:13:17 +0530

    Conditional Statement

---
+++ Part 2/Practice_Question 1/Traffic.js
@@ -0,0 +1,9 @@
+let color = "red";
+if (color === "red"){
+    console.log("YOU HAVE TO STOP !");}
+    if(color === "Yellow"){
+        console.log( " You have to wait for the green light to go"); // not shown
+    }
+    else(color === "green") //i wrote else but it should be if, on writing else  'you can go now' displayed but with if it is not displayed
+    {
+        console.log("You can go now");}
