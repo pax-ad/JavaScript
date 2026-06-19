19d9881237e7cbcc4fb776242551cee0d53c0182
pax-ad <ad007scrutinize@gmail.com>
2026-06-20 02:57:04 +0530

    Alert pop ups and input dialogue box

---
+++ Part 2/AlertPrompts.js
@@ -0,0 +1,16 @@
+alert("Something is wrong!")
+console.log("this is a simple log");
+//simple aleret pop up on the page.
+// can be uploaded through console too.
+
+console.error("this is an error message");
+//show an error icon in console
+
+console.warn("this is a warning msg");
+
+// prompt
+prompt("enter your name");
+
+let firstName = prompt("first name : ");
+console.log(firstName); //shows the firstname in console
+//ReferenceError: Can't find variable: firstname bcause i wrote firstName N case sensitive.
