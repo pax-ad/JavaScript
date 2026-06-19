d6adb98b5259b21ff0e0a70b525f8cf615e1fe05
pax-ad <ad007scrutinize@gmail.com>
2026-06-20 02:33:22 +0530

    switch operator problem

---
+++ Part 2/practice4.js
@@ -0,0 +1,34 @@
+//based upon switch statement
+
+let day = 4;
+
+switch(day){ // remember write s in small letter because it is case sensitive
+    
+    case "1":
+        console.log("Today is Monday");
+        break;
+    case "2":
+        console.log("Today is Tuesday");
+        break;
+    case "3":
+        console.log("Today is Wednesday");              
+        break;
+    case "4":
+        console.log("Today is Thursday");
+        break;
+    case "5":
+        console.log("Today is Friday");
+        break;
+    case "6":
+        console.log("Today is Saturday");
+        break;
+    case "7":
+        console.log("Today is Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
+
+
+
+
