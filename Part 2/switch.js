9d9f7a62d8fd7bcc5869e3daf56746a3b12d32af
pax-ad <ad007scrutinize@gmail.com>
2026-06-20 02:19:54 +0530

    Switch operator

---
+++ Part 2/switch.js
@@ -0,0 +1,18 @@
+let color = "red";
+
+switch(color){
+    case "red":
+        console.log("YOU HAVE TO STOP !");
+        break;
+    case "Yellow":
+        console.log( " You have to wait for the green light to go");
+        break;
+    case "green":
+        console.log("You can go now");
+        break;
+    default:
+        console.log("Invalid color");
+}
+
+// switch is used when we have to check multiple conditions based on the value of a variable
+// switch is more efficient than if-else when we have to check multiple conditions based on the value of a variable       
