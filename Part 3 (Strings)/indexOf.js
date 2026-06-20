5740b4926b135472cd0d1cd25f1451278302f881
pax-ad <ad007scrutinize@gmail.com>
2026-06-20 19:37:19 +0530

    IndexOf - Argument method

---
+++ Part 3 (Strings)/indexOf.js
@@ -0,0 +1,8 @@
+//indexOf method
+
+let m ="Tonystark,istheironman."
+let y = m.indexOf("is");
+console.log(m);
+console.log(y);
+
+// when write the space between words like " Tony stark is the iron man." it won't work.
