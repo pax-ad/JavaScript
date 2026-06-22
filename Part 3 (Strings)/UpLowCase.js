fdbc22990a8004bd331106cdd99da1909c6a9dda
pax-ad <ad007scrutinize@gmail.com>
2026-06-22 18:37:42 +0530

    Upper and lower case methods

---
+++ Part 3 (Strings)/UpLowCase.js
@@ -0,0 +1,7 @@
+let same = "Aman dawande";
+let p = same.toUpperCase();
+console.log(same); //this will print what in , let same = "Aman Dawande";
+console.log(p); // prints the uppercase
+
+let c = same.toLowerCase();
+console.log(c);
