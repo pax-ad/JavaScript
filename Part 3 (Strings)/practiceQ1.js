018c790bdcf06746bbafbebb6a45daaa893e971d
pax-ad <ad007scrutinize@gmail.com>
2026-06-24 22:55:27 +0530

    practice Question on slice,trim,etc

---
+++ Part 3 (Strings)/practiceQ1.js
@@ -0,0 +1,11 @@
+//q1
+let  q = "   help!";
+let w = q.trim().toUpperCase();
+console.log(q);
+console.log(w);
+
+//Q2
+let name ="Apnacollege";
+console.log(name.slice(4,9)); // ending index not included
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Our"));
