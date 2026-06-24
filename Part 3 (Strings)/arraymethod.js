f8ce45fe3556a806d7eb288528789c6d3b786625
pax-ad <ad007scrutinize@gmail.com>
2026-06-24 23:52:07 +0530

    push,pop,unshift,shift

---
+++ Part 3 (Strings)/arraymethod.js
@@ -0,0 +1,9 @@
+let cars = ["xuv 700","escalade","aston martin"];
+cars.push("bmW"); // bmw add at last
+cars.push("ferrari"); // ferrari add at last
+
+cars.pop(); // rmv from last 
+cars.unshift("lamborgini"); //add to start
+cars.shift(); // rmv from start
+console.log(cars);
+console.log(cars.length);
