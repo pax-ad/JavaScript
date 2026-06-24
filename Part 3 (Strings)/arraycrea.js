c0ff36bbfe0d72dd4c1f5a67f948ac8a142921c0
pax-ad <ad007scrutinize@gmail.com>
2026-06-24 23:13:35 +0530

    diff array intialization

---
+++ Part 3 (Strings)/arraycrea.js
@@ -0,0 +1,15 @@
+//empty array
+let empty = [];
+// this shows undefined
+
+empty.length;
+
+[].length;
+
+[1,2,3,45,4].length; 
+
+//accessing an index of an array element
+
+let info = ["Aman","shradhha",23,9.8];
+console.log(info[0]); // Aman
+console.log(info[0][0]); //first access "Aman" -> "A" which is at zero index.
