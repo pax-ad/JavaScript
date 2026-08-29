b433d7335169682cc5d7483891a272c4981be845
pax-ad <ad007scrutinize@gmail.com>
2026-08-30 01:18:19 +0530

    Loop practice

---
+++ Part_4/Favouritemovie.js
@@ -0,0 +1,9 @@
+//favourite movie js 
+let favouriteMovie = "Inception";
+
+let guessMovie = prompt("Guess my favourite movie:");
+
+while (guessMovie != favouriteMovie && guessMovie != "quit") {  
+    guessMovie = prompt("Guess my favourite movie:");
+    
+}
