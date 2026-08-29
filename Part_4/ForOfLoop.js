c77a9e1e9d7546476e1f5599c3d3d520a6501aa5
pax-ad <ad007scrutinize@gmail.com>
2026-08-30 02:14:13 +0530

    For of Loop in JS

---
+++ Part_4/ForOfLoop.js
@@ -0,0 +1,17 @@
+// for of loop 
+
+let fruits = ["apple","banana","chiku"];
+
+for(i of fruits){
+    console.log(i);
+}
+
+// here i is not an number 
+//but write like given below code
+for(fruit of fruits){
+    console.log(fruit);
+}
+
+for(char of "apnacollege"){
+    console.log(char);
+}
