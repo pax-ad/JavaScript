8caadb848abcc775aa10b8e68b2916b6966b6403
pax-ad <ad007scrutinize@gmail.com>
2026-09-03 22:58:02 +0530

    Documented object model

---
+++ part9/Dom.js
@@ -0,0 +1,4 @@
+//what is DOM?
+/*  The Document Object Model (DOM) is a programming interface for web documents.
+ It represents the structure of a document as a tree of objects, allowing developers to manipulate the content, structure, and style of web pages dynamically using programming languages like JavaScript. 
+ The DOM provides methods and properties to access and modify elements, attributes, and text within an HTML or XML document. */
