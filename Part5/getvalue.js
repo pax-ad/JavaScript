918e873e3b53bd7635222bfd44624ef5b7a13953
pax-ad <ad007scrutinize@gmail.com>
2026-09-10 18:22:25 +0530

    accessing the obj values

---
+++ Part5/getvalue.js
@@ -0,0 +1,13 @@
+// how to access the values inside the object
+
+//object named. student 
+const Student2 = { 
+    useraname : "Aman",
+    rollno : 24,
+    marks : 95
+
+};
+
+//accessing 
+Student["useraname"];
+Student.useraname;
