138356ced9bd70333cadaa62e3290d6979b6cfbd
pax-ad <ad007scrutinize@gmail.com>
2026-09-10 18:16:18 +0530

    obj literal ex.

---
+++ Part5/thread.js
@@ -0,0 +1,9 @@
+//creating a thread for twitter post having username , content , likes, reposts ,tags
+
+const thread = {
+    username : "Aman",
+    content : "Music",
+    likes : 100,
+    reposts : 30,
+    tages : "#Musicforever"
+}
