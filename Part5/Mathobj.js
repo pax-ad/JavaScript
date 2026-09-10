7a048fe8fc5d133d032fee7aa27ea354dcbab444
pax-ad <ad007scrutinize@gmail.com>
2026-09-10 19:17:04 +0530

    Math methods in js

---
+++ Part5/Mathobj.js
@@ -0,0 +1,23 @@
+//math object
+//js already of collection of properties and methods
+//method we are gonna focuse on
+//Math.abs(n)
+//Math.pow(a,b)
+//Math.floor(n)
+//Math.ceil(n)
+//Matn.random
+
+let num = Math.random(); // it gives us anything b/w 0 to 1.
+
+num = num * 10 ; //bringing b/w 1 to 10.
+
+num = Math.floor(num); // round off to the nearst small integer
+
+num = num + 1;
+
+// one step
+
+let number = Math.floor(Math.random()*10) + 1;
+
+/*adding one because we are never gonna see the 1
+ since random will generate only b/w (0 to 1) in which one is not included * 10 will not give us range from 1 to 9 so for 1 to 10 we are adding +1 for range : 1 to 10;  
