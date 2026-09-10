f059d91cbd00275b360f528bb23643d5099d334f
pax-ad <ad007scrutinize@gmail.com>
2026-09-10 18:33:32 +0530

    add,update an obj

---
+++ Part5/adduupdate.js
@@ -0,0 +1,25 @@
+//obj
+
+const Place =
+ {
+    name : "vidisha",
+    age : "32",
+    marks : 75,
+    city : "bhopal"
+
+}
+
+//changing city to indore
+
+Place.city = "Indore";
+
+//adding new property
+
+Place.gender = "Female";
+
+//changning the marks 
+
+Place.marks = "B tier";
+
+console.log(Place);
+
