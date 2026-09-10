1689ff11449e500cb953776bb8471846c59ec6a4
pax-ad <ad007scrutinize@gmail.com>
2026-09-10 18:40:32 +0530

    objects of object , operations

---
+++ Part5/objectofobject.js
@@ -0,0 +1,24 @@
+//object of objects 
+const classinfo = {
+    aman : {
+        grade : "A+",
+        city : "Delhi"
+    },
+
+    shradha : { 
+        grade : "A",
+        city : "Pune"
+    },
+
+    karan : {
+        grade : "0",
+        city : "Mumbai"
+    }
+};
+
+//the same can be sotored inside of an array just replace the curly brackets with the square brackets.
+
+/* classinfo[0] will show key value pair/obj aman
+
+for making changes classinfo[0].marks = 100 
+ similar for other objects */
